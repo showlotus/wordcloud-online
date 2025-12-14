f4fb9ad94971837a1f470fac315b4fe3902470b2
showlotus <showlotus@users.noreply.github.com>
2025-12-14 14:52:47 +0000

    Deploying to gh-pages from @ showlotus/wordcloud-online@5861ed8f1d7d07a3b41b2fbad4719b0c0b97570d 🚀

--- src/lib/genColor.ts
+++
@@ -1,8 +0,0 @@
-import convert from 'color-convert'
-
-export default function genColor(themeColor: string) {
-  const c = convert.hex.rgb(themeColor)
-  return `rgb(${c.map(
-    (v) => (Math.random() < 0.5 ? -1 : 1) * Math.floor(Math.random() * 80) + v
-  )})`
-}
